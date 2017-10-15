98775e8c5cccfef9a4c64f308d4e2f6aee40ed74
Sainathan Iyer <sainathaniyer@gmail.com>
2017-10-15 12:57:47 +0530

    Add files via upload

---
+++ Blinking_LED.ino
@@ -0,0 +1,14 @@
+//Blinking LED
+const int LED = 13;//LED connected to digital pin 13
+void setup()//this specifies that setup function is defined now 
+{
+  pinMode(LED, OUTPUT);//tells arduino how to use the pin like here its sets the digital pin as output.
+}
+void loop()
+{
+  digitalWrite(LED, HIGH);   // turns the LED on 
+  delay(1000);      // waits for a second 
+  digitalWrite(LED, LOW);      // turns the LED off 
+  delay(1000); // waits for a second 
+}
+
